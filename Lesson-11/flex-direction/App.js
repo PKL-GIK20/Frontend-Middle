91d1e7b36b3e02685ee2367091ac3bd1b623a677
lamdarv <lamdarvs@gmail.com>
2023-08-08 14:45:42 +0700

    add flex-direction

---
+++ Lesson-11/flex-direction/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import FlexDirectionBasics from './FlexDirectionBasics';
+
+export default function App() {
+  return (
+    <FlexDirectionBasics />
+  );
+}
+
