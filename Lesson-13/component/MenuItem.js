70c7a085cadbc462106632c0fbd5d534c6e641d2
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2023-08-08 15:25:48 +0700

    Update MenuItem.js

--- Lesson-13/component/MenuItem.js
+++ Lesson-13/component/MenuItem.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
-function MenuItem({ id, name, description, price, image, isSelected, onPress }) {
+function MenuItem({ id, name, description, price, image, quantity, onQuantityChange, onAddToCart, onDeleteItem }) {
   return (
-    <TouchableOpacity
-      style={[styles.container, isSelected ? styles.selectedContainer : null]}
-      onPress={onPress}
-    >
+    <View style={styles.container}>
       <Image source={image} style={styles.image} />
       <View style={styles.infoContainer}>
         <Text style={styles.name}>{name}</Text>
         <Text style={styles.description}>{description}</Text>
-        <Text style={styles.price}>Rp.{price}</Text>
+        <Text style={styles.price}>Rp.{price.toFixed(2)}</Text>
       </View>
-    </TouchableOpacity>
+      <View style={styles.quantityContainer}>
+        <TouchableOpacity
+          style={styles.quantityButton}
+          onPress={() => {
+            if (quantity > 0) {
+              onQuantityChange(quantity - 1);
+            }
+          }}
+        >
+          <Text style={styles.quantityButtonText}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.quantityValue}>{quantity}</Text>
+        <TouchableOpacity style={styles.quantityButton} onPress={onAddToCart}>
+          <Text style={styles.quantityButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
   );
 }
 
@@ -49,6 +63,28 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#333',
     fontWeight: 'bold',
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantityButton: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 15,
+    marginHorizontal: 5,
+  },
+  quantityButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#555',
+  },
+  quantityValue: {
+    fontSize: 16,
+    marginHorizontal: 10,
   },
 });
 
