8bece6ae3da7c0a36635bb835d424617f0ccc129
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2023-08-08 15:04:36 +0700

    Update App.js

--- Lesson-13/App.js
+++ Lesson-13/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './component/HomeScreen';
 import ShoppingList from './component/ShoppingList';
 import AboutScreen from './component/AboutScreen';
+import OrderSummary from './component/OrderSummary';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
         <Stack.Screen name="ShoppingList" component={ShoppingList} options={{ title: 'Menu List' }} />
         <Stack.Screen name="About" component={AboutScreen} options={{ title: 'About Us' }} />
+        <Stack.Screen name="OrderSummary" component={OrderSummary} options={{ title: 'Pesanan' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
