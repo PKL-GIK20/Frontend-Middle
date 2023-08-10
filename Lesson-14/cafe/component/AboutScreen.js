b2988e480afe8f9327d22a158ad01b650f92af6c
lamdarv <lamdarvs@gmail.com>
2023-08-10 15:35:32 +0700

    add transformation in AboutScreen.js

--- Lesson-14/cafe/component/AboutScreen.js
+++ Lesson-14/cafe/component/AboutScreen.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, Animated } from 'react-native';
-import StarRating from 'react-native-star-rating';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, Animated, Easing } from "react-native";
+import StarRating from "react-native-star-rating";
 
 function AboutScreen({ navigation }) {
   const [rating, setRating] = useState(0);
   const onStarRatingPress = (rating) => {
-    setRating(rating); 
+    setRating(rating);
   };
+  // Membuat nilai animasi dengan nilai awal 0
+  const value = new Animated.Value(0);
+
+  // Menjalankan animasi timing dengan durasi 2 detik dan fungsi easing linear
+  useEffect(() => {
+    Animated.timing(value, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/TNI.jpg')} style={styles.image} />
+      {/* <Image source={require("../assets/TNI.jpg")} style={styles.image} /> */}
+      <Animated.Image source={require('../assets/TNI.jpg')} style={[styles.image, {transform: [{rotate: value.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '72000deg']
+      })}]}]} />
       <Text style={styles.title}>Tentang Kami</Text>
-      <Text style={styles.description}>Kami tak pernah meragukan pelanggan, meskipun permintaan yang aneh aneh.</Text>
+      <Text style={styles.description}>
+        Kami tak pernah meragukan pelanggan, meskipun permintaan yang aneh aneh.
+      </Text>
       <View style={styles.ratingContainer}>
         <Text style={styles.ratingText}>Rating: </Text>
         <StarRating
@@ -31,18 +49,18 @@ function AboutScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    alignItems: 'center',
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
     padding: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   description: {
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
   },
   image: {
@@ -52,8 +70,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   ratingContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   ratingText: {
     fontSize: 18,
