0bdda9f161b8d9382e01cc656936da9c0b8192b8
lamdarv <lamdarvs@gmail.com>
2023-08-23 10:42:06 +0700

    add error handling and loading indicator

--- Lesson-27/flutter_firebase_sub_2/lib/screens/login_screen.dart
+++ Lesson-27/flutter_firebase_sub_2/lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_sub_2/services/auth_service.dart';
 
 class LoginScreen extends StatelessWidget {
   TextEditingController emailController = TextEditingController();
@@ -38,7 +40,22 @@ class LoginScreen extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               child: ElevatedButton(
                 onPressed: ()async{
-
+                  if(emailController.text == "" || passwordController.text == ""){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          "All fields are required!",
+                          style: TextStyle(backgroundColor: Colors.red),
+                        ),
+                      ),
+                    );
+                  } else {
+                    User? result = await AuthService().login(emailController.text, passwordController.text, context);
+                    if (result != null){
+                      print("Success");
+                      print(result.email);
+                    }
+                  }
                 }, 
                 child: Text("Submit", style: TextStyle(
                   fontSize: 25, fontWeight: FontWeight.bold
--- Lesson-27/flutter_firebase_sub_2/lib/screens/register_screen.dart
+++ Lesson-27/flutter_firebase_sub_2/lib/screens/register_screen.dart
@@ -1,11 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_sub_2/screens/login_screen.dart';
+import 'package:flutter_firebase_sub_2/services/auth_service.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   TextEditingController emailController = TextEditingController();
+
   TextEditingController passwordController = TextEditingController();
+
   TextEditingController confirmPasswordController = TextEditingController();
+
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +57,57 @@ class RegisterScreen extends StatelessWidget {
             ),
             SizedBox(height: 30,),
 
-            Container(
+            loading? CircularProgressIndicator(): Container(
               height: 50,
               width: MediaQuery.of(context).size.width,
               child: ElevatedButton(
-                onPressed: ()async{
-
-                }, 
-                child: Text("Submit", style: TextStyle(
-                  fontSize: 25, fontWeight: FontWeight.bold
-                ),),
+                onPressed: () async {
+                  setState(() {
+                    loading = true;
+                  });
+                  if (emailController.text == "" || passwordController.text == "") {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          "All fields are required!",
+                          style: TextStyle(backgroundColor: Colors.red),
+                        ),
+                      ),
+                    );
+                  } else if (passwordController.text != confirmPasswordController.text) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          "Password doesn't match!",
+                          style: TextStyle(backgroundColor: Colors.red),
+                        ),
+                      ),
+                    );
+                  } else {
+                    User? result = await AuthService().register(
+                      emailController.text,
+                      passwordController.text,
+                      context, // Pass the BuildContext as the third argument
+                    );
+                    if (result != null) {
+                      print("Success");
+                      print(result.email);
+                    }
+                  }
+                  setState(() {
+                    loading = false;
+                  });
+                },
+                child: Text(
+                  "Submit",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
+
 
             SizedBox(height: 20,),
             TextButton(onPressed: (){
