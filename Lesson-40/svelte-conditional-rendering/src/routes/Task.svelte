51e7e8ac46d7d95758a8caa8ec4f1590409a67f3
lamdarv <lamdarvs@gmail.com>
2023-08-23 16:26:06 +0700

    add implementasi conditional rendering svelte

---
+++ Lesson-40/svelte-conditional-rendering/src/routes/Task.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let task;
+</script>
+
+<div class="task">
+  <input type="checkbox" bind:checked={task.completed} />
+  <span class:completed={task.completed}>{task.title}</span>
+</div>
+
+<style>
+  .completed {
+    text-decoration: line-through;
+  }
+
+  .task {
+    margin: 0.5rem 0;
+  }
+</style>
